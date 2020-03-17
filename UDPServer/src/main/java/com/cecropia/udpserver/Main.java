27708bf5cdda5af463a1ec909f5fd96693704158
Roy Quesada <rquesada@harper.agency>
2020-03-16 21:33:01 -0600

    Simulate devices

--- UDPServer/src/main/java/com/cecropia/udpserver/Main.java
+++ UDPServer/src/main/java/com/cecropia/udpserver/Main.java
@@ -5,10 +5,39 @@
  */
 package com.cecropia.udpserver;
 
+import java.io.*;
+import java.net.*;
+
 /**
  *
  * @author royquesada
  */
 public class Main {
+    public static void main(String args[]) throws Exception
+      {
+         DatagramSocket serverSocket = new DatagramSocket(9876);
+            byte[] receiveData = new byte[1024];
+            byte[] sendData = new byte[1024];
+            while(true)
+               {
+                  DatagramPacket receivePacket = new DatagramPacket(receiveData, receiveData.length);
+                  serverSocket.receive(receivePacket);
+                  String sentence = new String( receivePacket.getData());
+                  System.out.println("RECEIVED: " + sentence);
+                  InetAddress IPAddress = receivePacket.getAddress();
+                  int port = receivePacket.getPort();
+                  
+                  String capitalizedSentence = sentence.toUpperCase();
+                  if (sentence.contains("connect:")){
+                      capitalizedSentence="connections:192.168.100.1:192.168.100.2:192.168.100.3";
+                  }
+                  
+                  
+                  sendData = capitalizedSentence.getBytes();
+                  DatagramPacket sendPacket =
+                  new DatagramPacket(sendData, sendData.length, IPAddress, port);
+                  serverSocket.send(sendPacket);
+               }
+      }
     
 }
